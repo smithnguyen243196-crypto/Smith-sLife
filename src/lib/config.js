63bb25972f16179a9a7b5766dd48e68b882b003a
smithnguyen243196-crypto <smithnguyen243196@gmail.com>
2026-06-11 12:42:41 +0700

    Update config.js

--- src/lib/config.js
+++ src/lib/config.js
@@ -20,5 +20,6 @@ export const EVENING_Q = [
 ];
 
 export const DENOMS = [500000, 200000, 100000, 50000, 20000, 10000, 5000, 2000, 1000];
-export const REASON_SUGGEST = ["Xăng", "Quà", "Nợ công ty", "Ship"];
+export const REASON_SUGGEST = ["Chi bớt khách", "Xăng", "Quà", "Nợ công ty", "Ship"];
+export const PERSON_SUGGEST = ["a.Tài", "Hải", "a.Thắng", "Uyên", "Đô", "Gin", "Như Ý", "c.Hân", "Tom", "a.Linh", "Bi", "Bình"];
 export const VI_CATEGORIES = ["Ăn uống", "Điện nước", "Đưa vợ", "Xăng xe", "Mua sắm", "Y tế", "Học tập", "Giải trí", "Lương/Thu nhập", "Khác"];
